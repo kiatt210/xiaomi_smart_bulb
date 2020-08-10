8baf16b8ab5995ecbeff1284951dc0602f25d4e0
kiatt210 <kiatt@freemail.hu>
2020-08-10 08:02:14 +0200

    Color setting works

--- light.py
+++ light.py
@@ -3,14 +3,17 @@ from homeassistant.components.light import (LightEntity,
     ATTR_COLOR_TEMP,
     SUPPORT_COLOR_TEMP,
     SUPPORT_BRIGHTNESS,
+    SUPPORT_COLOR,
     ATTR_HS_COLOR,
-    ATTR_COLOR_TEMP)
+    ATTR_COLOR_TEMP,
+    ATTR_RGB_COLOR)
 
 import voluptuous as vol
 from math import ceil
 from typing import Optional
 from functools import partial
 import logging
+from typing import Tuple
 
 from homeassistant.const import CONF_FILENAME, CONF_HOST, CONF_TOKEN, CONF_NAME
 from homeassistant.components.light import PLATFORM_SCHEMA
@@ -21,6 +24,8 @@ from homeassistant.util.color import (
     color_temperature_kelvin_to_mired as kelvin_to_mired,
     color_temperature_mired_to_kelvin as mired_to_kelvin,
 )
+
+from homeassistant.util import color
 
 _LOGGER = logging.getLogger(__name__)
 DEFAULT_NAME = "Xiaomi Smart Bulb"
@@ -41,7 +46,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
         """Set up the light from config."""
-        from miio import Device, DeviceException, Yeelight
+        from miio import Device, DeviceException, Yeelight, PhilipsMoonlight
 
         if DATA_KEY not in hass.data:
             hass.data[DATA_KEY] = {}
@@ -57,29 +62,29 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
         lights = []
 
-        light = Yeelight(host, token)
-        device = XiaomiSmartBulb(name, light, unique_id)
+        yeelight = Yeelight(host, token)
+        device = XiaomiSmartBulb(name, yeelight, unique_id)
 
         lights.append(device)
         add_entities(lights)
-        hass.data[DATA_KEY][host] = light
+        hass.data[DATA_KEY][host] = yeelight
 
 
 class XiaomiSmartBulb(LightEntity):
 
 
-    def __init__(self, name, device, unique_id):
+    def __init__(self, name, yeelight_device, unique_id):
         """Initialize the light device."""
         self._name = name
-        self._device = device
+        self._yeelight_device = yeelight_device
         self._unique_id = unique_id
         self._state = None
         self._color_temp = None
+        self._rgb = None
         self._model = "xiaomi_smart_led"
         self._brightness = None
         self._state_attrs = {ATTR_MODEL: self._model}
         self._available = False
-        _LOGGER.error("My name :%s", self._name)
 
     @staticmethod
     def translate(value, left_min, left_max, right_min, right_max):
@@ -87,20 +92,25 @@ class XiaomiSmartBulb(LightEntity):
         left_span = left_max - left_min
         right_span = right_max - right_min
         value_scaled = float(value - left_min) / float(left_span)
-        _LOGGER.info("Translate result :%s", int(right_min + (value_scaled * right_span)) )
+        _LOGGER.debug("Translate result :%s", int(right_min + (value_scaled * right_span)) )
         return int(right_min + (value_scaled * right_span)) 
 
     @staticmethod
     def translate_to_value(percent, min, max):
         """Translate percentage to value """
         scale = max -min
-        _LOGGER.info("Translate %s, %s, %s result: %s", percent,min,max, int (value = percent * (scale/100)))
+        _LOGGER.debug("Translate %s, %s, %s result: %s", percent,min,max, int (value = percent * (scale/100)))
         return int (value = percent * (scale/100))
     
+    @staticmethod
+    def rgb_to_int(x: Tuple[int, int, int]) -> int:
+        """Return an integer from RGB tuple."""
+        return int(x[0] << 16 | x[1] << 8 | x[2])
+
     @property
     def supported_features(self):
         """Return the supported features."""
-        return SUPPORT_BRIGHTNESS | SUPPORT_COLOR_TEMP
+        return SUPPORT_BRIGHTNESS | SUPPORT_COLOR_TEMP | SUPPORT_COLOR
 
     @property
     def should_poll(self):
@@ -110,7 +120,7 @@ class XiaomiSmartBulb(LightEntity):
     @property
     def available(self):
         """Return true when state is known."""
-        _LOGGER.info("Is available?: %s", self._available)
+        _LOGGER.debug("Is available?: %s", self._available)
         return self._available
 
     @property
@@ -121,7 +131,7 @@ class XiaomiSmartBulb(LightEntity):
     @property
     def is_on(self):
         """Return true if light is on."""
-        _LOGGER.info("Is on?: %s", self._state)
+        _LOGGER.debug("Is on?: %s", self._state)
         return self._state
 
     @property
@@ -131,13 +141,13 @@ class XiaomiSmartBulb(LightEntity):
     @property
     def color_temp(self):
         """Return the CT color value in mireds."""
-        _LOGGER.info("Return color temp: %s", self._color_temp)
+        _LOGGER.debug("Return color temp: %s", self._color_temp)
         return self._color_temp
     
     @property
     def brightness(self):
         """Return the brightness of this light between 0..255."""
-        _LOGGER.info("Return brightness: %s", self._brightness)
+        _LOGGER.debug("Return brightness: %s", self._brightness)
         return self._brightness
 
     @property
@@ -151,30 +161,27 @@ class XiaomiSmartBulb(LightEntity):
         return 6500
 
     async def async_update(self):
-        _LOGGER.info("async_update")
+        _LOGGER.debug("async_update")
         """Fetch state from the device."""
         from miio import DeviceException
 
         try:
-            state = await self.hass.async_add_executor_job(self._device.status)
+            yeelight_state = await self.hass.async_add_executor_job(self._yeelight_device.status)
         except DeviceException as ex:
             self._available = False
             _LOGGER.error("Got exception while fetching the state: %s", ex)
             return
 
-        _LOGGER.info("Got new state: %s", state)        
+        _LOGGER.debug("Got new yeelight state: %s", yeelight_state)
         self._available = True
-        self._state = state.is_on
-        self._brightness = ceil((255 / 100.0) * state.brightness)
-        self._color_temp = state.color_temp
-        #self._color_temp = self.translate(
-        #    state.color_temp, CCT_MIN, CCT_MAX, self.max_mireds, self.min_mireds
-        #)
-
+        self._state = yeelight_state.is_on
+        self._brightness = ceil((255 / 100.0) * yeelight_state.brightness)
+        self._color_temp = yeelight_state.color_temp
+        self._rgb = yeelight_state.rgb
 
     async def _try_command(self, mask_error, func, *args, **kwargs):
         """Call a light command handling error messages."""
-        _LOGGER.error("Start try comman")
+        _LOGGER.debug("Start try command")
         from miio import DeviceException
 
         try:
@@ -182,7 +189,7 @@ class XiaomiSmartBulb(LightEntity):
                 partial(func, *args, **kwargs)
             )
 
-            _LOGGER.error("Response received from light: %s", result)
+            _LOGGER.debug("Response received from light: %s", result)
 
             return result == SUCCESS
         except DeviceException as exc:
@@ -201,9 +208,16 @@ class XiaomiSmartBulb(LightEntity):
         if ATTR_BRIGHTNESS in kwargs:
             brightness = kwargs[ATTR_BRIGHTNESS]
             percent_brightness = ceil(100 * brightness / 255.0)
+        
+        if ATTR_RGB_COLOR in kwargs:
+            rgb = kwargs[ATTR_RGB_COLOR]
+        
+        if ATTR_HS_COLOR in kwargs:
+            hs_color = kwargs[ATTR_HS_COLOR]
+            rgb = color.color_hs_to_RGB(*hs_color)
 
         if ATTR_BRIGHTNESS in kwargs and ATTR_COLOR_TEMP in kwargs:
-            _LOGGER.error(
+            _LOGGER.debug(
                 "Setting brightness and color temperature: "
                 "%s %s%%, %s mireds, %s%% cct",
                 brightness,
@@ -214,20 +228,17 @@ class XiaomiSmartBulb(LightEntity):
 
             result = await self._try_command(
                 "Setting brightness and color temperature failed: " "%s bri, %s cct",
-                self._device.set_brightness_and_color_temperature,
+                self._yeelight_device.set_brightness_and_color_temperature,
                 percent_brightness,
                 percent_color_temp,
             )
 
             if result:
-                #self._color_temp = self.translate(
-                #    state.color_temp, CCT_MIN, CCT_MAX, self.max_mireds, self.min_mireds
-                #)
                 self._color_temp = state.color_temp
                 self._brightness = brightness
 
         elif ATTR_COLOR_TEMP in kwargs:
-            _LOGGER.error(
+            _LOGGER.debug(
                 "Setting color temperature: " "%s mireds, %s%% cct",
                 color_temp,
                 percent_color_temp,
@@ -235,25 +246,34 @@ class XiaomiSmartBulb(LightEntity):
 
             result = await self._try_command(
                 "Setting color temperature failed: %s cct",
-                self._device.set_color_temp,
+                self._yeelight_device.set_color_temp,
                 color_temp,
             )
 
             if result:
-                #self._color_temp = self.translate(
-                #    state.color_temp, CCT_MIN, CCT_SMAX, self.max_mireds, self.min_mireds
-                #)
                 self._color_temp = color_temp                                  
+
+        elif ATTR_RGB_COLOR in kwargs or ATTR_HS_COLOR in kwargs:
+            rgb_int=int(rgb[0] << 16 | rgb[1] << 8 | rgb[2])
+            _LOGGER.debug("Set rgb color to:set_rgb '["+str(rgb_int)+",\"smooth\",500]'")
+
+            result = await self._try_command(
+                "Setting rgb failed: %s",
+                self._yeelight_device.set_rgb, rgb
+            )
+
+            if result:
+                self._rgb = rgb
 
         elif ATTR_BRIGHTNESS in kwargs:
             brightness = kwargs[ATTR_BRIGHTNESS]
             percent_brightness = ceil(100 * brightness / 255.0)
 
-            _LOGGER.error("Setting brightness: %s %s%%", brightness, percent_brightness)
+            _LOGGER.debug("Setting brightness: %s %s%%", brightness, percent_brightness)
 
             result = await self._try_command(
                 "Setting brightness failed: %s",
-                self._device.set_brightness,
+                self._yeelight_device.set_brightness,
                 percent_brightness,
             )
 
@@ -261,15 +281,15 @@ class XiaomiSmartBulb(LightEntity):
                 self._brightness = brightness
 
         else:
-            await self._try_command("Turning the light on failed.", self._device.on)
+            await self._try_command("Turning the light on failed.", self._yeelight_device.on)
 
     def turn_off(self, **kwargs):
         """Turn the device off."""
-        _LOGGER.error("Turn_off")
-        self._try_command("Turning the light off failed.", self._device.off)
+        _LOGGER.debug("Turn_off")
+        self._try_command("Turning the light off failed.", self._yeelight_device.off)
         self._state=False
     async def async_turn_off(self, **kwargs):
         """Turn device off."""
-        _LOGGER.error("Async turn_off")
-        await self._try_command("Turning the light off failed.", self._device.off)
+        _LOGGER.debug("Async turn_off")
+        await self._try_command("Turning the light off failed.", self._yeelight_device.off)
         self._state=False
